ea0aa17a18bc08b64860c7fdba608da747f2de1d
Hassan Farahani <h.farahani@bahartel.ir>
2019-11-26 18:46:52 +0330

    call sale api added

--- transactions/views.py
+++ transactions/views.py
@@ -29,33 +29,6 @@ def mcci_call_sale(request):
             tell_charger=tell_charger,
             charge_type=charge_type
         )
-        call_response_type, call_response_description = MCI.call_sale(
-            top_up.tell_num,
-            top_up.tell_charger,
-            top_up.amount,
-            top_up.charge_type,
-        )
-        success = top_up.after_call(call_response_type, call_response_description)
-        if success:
-            data = {
-                "message": "success: request successfully submitted",
-                "message_fa": "موفقیت: درخواست با موفقیت ثبت شد",
-                "code": 200,
-                "status": 200,
-                "provider_id": top_up.provider_id
-            }
-            return Response(data, status=HTTP_200_OK)
-        else:
-            data = {
-                "message": "error: failed to submit request",
-                "message_fa": "خطا: خطا در ثبت درخواست",
-                "code": 301,
-                "status": 200,
-                "call_response_type": top_up.call_response_type,
-                "call_response_description": top_up.call_response_description
-            }
-            return Response(data, status=HTTP_200_OK)
-
     except Exception:
         data = {
             "message": "error: invalid parameters",
@@ -64,3 +37,32 @@ def mcci_call_sale(request):
             "status": 400,
         }
         return Response(data, status=HTTP_400_BAD_REQUEST)
+    
+    call_response_type, call_response_description = MCI.call_sale(
+        top_up.tell_num,
+        top_up.tell_charger,
+        top_up.amount,
+        top_up.charge_type,
+    )
+    success = top_up.after_call(call_response_type, call_response_description)
+    if success:
+        data = {
+            "message": "success: request successfully submitted",
+            "message_fa": "موفقیت: درخواست با موفقیت ثبت شد",
+            "code": 200,
+            "status": 200,
+            "provider_id": top_up.provider_id
+        }
+        return Response(data, status=HTTP_200_OK)
+    else:
+        data = {
+            "message": "error: failed to submit request",
+            "message_fa": "خطا: خطا در ثبت درخواست",
+            "code": 301,
+            "status": 200,
+            "call_response_type": top_up.call_response_type,
+            "call_response_description": top_up.call_response_description
+        }
+        return Response(data, status=HTTP_200_OK)
+
+
