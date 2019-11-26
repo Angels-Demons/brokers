6ec65310dbb4cfb5df269fb350d263c4b3fd8234
Hamid <hamidmg22@yahoo.com>
2019-11-26 14:43:31 +0330

    urls and login

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import refresh_jwt_token
+from accounts.views import *
+
+
+urlpatterns = {
+    url(r'^BrokerLogin/$', BrokerLogin.as_view()),
+    url(r'^RefreshToken/', refresh_jwt_token),
+}
+urlpatterns = format_suffix_patterns(urlpatterns)
