0a97d9019bc2eed49319a9c9004d81cf44b5ab1e
Reza Ghanbar <reza.qanbar96@gmail.com>
2019-11-27 20:30:43 +0330

    remove 0x

--- interface/API.py
+++ interface/API.py
@@ -174,9 +174,6 @@ class MCI:
     @staticmethod
     def behsa_hash(hash_string):
         byte_hash = hash_string.encode()
-        # md5hash = "0x" + hashlib.md5(byte_hash)).replace("-", "").lower()
         md5hash = hashlib.md5(byte_hash).hexdigest()
-        return "0x" + md5hash.replace("-", "")
-        # Finally = binascii.b2a_hex(hashlib.md5(byte_hash).digest())
-        # DeviceIdentity = "0x" + Finally.decode('utf-8').replace("-", "")
-        # return DeviceIdentity
+        return  md5hash.replace("-", "")
+
