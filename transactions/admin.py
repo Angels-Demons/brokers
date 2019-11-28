90d32db11a397f633ef504c2ec19b7ada6252b92
Reza Ghanbar <reza.qanbar96@gmail.com>
2019-11-28 13:56:33 +0330

    register model

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from transactions.models import TopUp
+from transactions.models import TopUp,Package,ProvidersToken
 
 admin.site.register(TopUp)
+
+admin.site.register(ProvidersToken)
+admin.site.register(Package)
