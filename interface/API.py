1e37ea1055a556f41fd61cf5343a6b4867d92dd8
Reza Ghanbar <reza.qanbar96@gmail.com>
2019-11-28 16:55:48 +0330

    011

--- interface/API.py
+++ interface/API.py
@@ -41,6 +41,22 @@ class MCI:
     def charge_call_sale(self, tel_num, tel_charger, amount, charge_type):
         try:
             print("******** Start CallSale Request ***** ")
+            try:
+                import http.client as http_client
+            except ImportError:
+                # Python 2
+                import httplib as http_client
+            http_client.HTTPConnection.debuglevel = 1
+
+            # You must initialize logging, otherwise you'll not see debug output.
+            logging.basicConfig()
+            logging.getLogger().setLevel(logging.DEBUG)
+            requests_log = logging.getLogger("requests.packages.urllib3")
+            requests_log.setLevel(logging.DEBUG)
+            requests_log.propagate = True
+
+
+            
             base64Authorization = self.localBasic(self.behsa_username, self.behsa_generated_pass)
             header = {'Content-type': 'application/json','Authorization': base64Authorization,'charset':'UTF-8'}
             data = {
