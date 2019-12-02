cb95bade738825dfefb138bbceedc0bd7dcb5cbb
Reza Ghanbar <reza.qanbar96@gmail.com>
2019-12-02 09:38:26 +0330

    004

--- transactions/views.py
+++ transactions/views.py
@@ -423,9 +423,9 @@ class TestApi58(BaseAPIView):
                 "code": codes.inactive_broker,
             }
             return Response(data, status=status.HTTP_200_OK)
+        exe_response_type_2, exe_response_description_2 = MCI().behsa_package_credit()
 
         exe_response_type_1, exe_response_description_1 = MCI().behsa_charge_credit()
-        exe_response_type_2, exe_response_description_2 = MCI().behsa_package_credit()
 
         data = {
             "message": "Request successfully executed",
