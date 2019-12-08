1d405459456adc0a84c8fafa7e265575c5a4ac2a
amirrezaFathi <amirreza.f.amirreza@gmail.com>
2019-12-08 20:28:59 +0330

    behsa added

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,11 +1,30 @@
 from django.contrib.auth.models import User
 
+from accounts.admin import is_admin
 from accounts.models import Broker
+from interface.API import MCI
 
 
 def test(request):
+    dictionary = {}
+    if request.user.is_superuser or is_admin(request.user):
+        exe_response_type_1, exe_response_description_1 = MCI().behsa_charge_credit()
+        exe_response_type_2, exe_response_description_2 = MCI().behsa_package_credit()
+
+        if exe_response_type_1 == 0:
+            dictionary['Charge_Credit'] = exe_response_description_1
+        else:
+            dictionary['Charge_Credit'] = "اتصال با سرور برقرار نیست"
+        if exe_response_type_2 == 0:
+            dictionary['Package_Credit'] = exe_response_description_2
+        else:
+            dictionary['Package_Credit'] = "اتصال با سرور برقرار نیست"
+
     try:
-        credit = Broker.objects.get(user=request.user)
-        return {'user_Broker': 1}
+        Broker.objects.get(user=request.user)
+        dictionary['user_Broker'] = 1
+
     except:
-        return {}
+        pass
+
+    return dictionary
