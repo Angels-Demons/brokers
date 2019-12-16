fd4195184bfa5ed2213e4513fb44a159ccdcdf0f
Hamid <hamidmg22@yahoo.com>
2019-12-16 23:32:11 +0330

    race test without lock

--- transactions/views.py
+++ transactions/views.py
@@ -664,32 +664,30 @@ class RaceTest(BaseAPIView):
 
     @staticmethod
     def post(request):
-        with transaction.atomic():
-            try:
-                sleep(0.4)
-                broker = Broker.objects.get(user=request.user)
-                operator_access = broker.operatoraccess_set.select_for_update().get(operator=Operator.MCI.value)
-                operator_access.charge(1, True)
-                data = {
-                    "message": "Request successfully executed",
-                    "message_fa": "درخواست با موفقیت اجرا شد",
-                    "code": codes.successful,
-                }
-                return Response(data, status=status.HTTP_200_OK)
-            except OperatorAccess.DoesNotExist as e:
-                data = {
-                    "message": "Broker does not have access for this action",
-                    "message_fa": "خطا: کاربر دسترسی لازم برای این عملیات را ندارد.",
-                    "code": codes.invalid_access,
-                }
-                return Response(data, status=status.HTTP_400_BAD_REQUEST)
-            except Exception as e:
-                data = {
-                    "message": "Broker does not have access for this action",
-                    "message_fa": "خطا: کاربر دسترسی لازم برای این عملیات را ندارد.",
-                    "code": codes.invalid_access,
-                }
-                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            broker = Broker.objects.get(user=request.user)
+            operator_access = broker.operatoraccess_set.get(operator=Operator.MCI.value)
+            operator_access.charge(1, True)
+            data = {
+                "message": "Request successfully executed",
+                "message_fa": "درخواست با موفقیت اجرا شد",
+                "code": codes.successful,
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except OperatorAccess.DoesNotExist as e:
+            data = {
+                "message": "Broker does not have access for this action",
+                "message_fa": "خطا: کاربر دسترسی لازم برای این عملیات را ندارد.",
+                "code": codes.invalid_access,
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            data = {
+                "message": "Broker does not have access for this action",
+                "message_fa": "خطا: کاربر دسترسی لازم برای این عملیات را ندارد.",
+                "code": codes.invalid_access,
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ActivePackages(BaseAPIView):
