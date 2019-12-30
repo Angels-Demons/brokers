69d2e028cc39892c51ed58178f09ee70a1fa9807
Reza Ghanbar <reza.qanbar96@gmail.com>
2019-12-30 12:01:16 +0330

    add Vat to package query WS

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,12 +5,17 @@ from transactions.models import Package, Operator
 
 class PackageSerializer(serializers.ModelSerializer):
     operator_name = serializers.SerializerMethodField('operator_name_method')
+    PackageCostWithVat = serializers.SerializerMethodField('get_PackageCostWithVat')
 
     def operator_name_method(self, obj):
         return Operator.farsi(obj.operator)
 
+    def get_PackageCostWithVat(self, obj):
+        return int(1.09 * int(obj.amount))
+
     class Meta:
         model = Package
-        fields = ['package_type', 'operator', 'name', 'amount','system', 'description', 'operator_name','package_duration']
+        fields = ['package_type', 'operator', 'name', 'amount', 'PackageCostWithVat', 'system', 'description',
+                  'operator_name', 'package_duration']
 
 
